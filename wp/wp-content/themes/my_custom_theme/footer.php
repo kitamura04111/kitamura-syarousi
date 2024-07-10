f825d00078d8d7f1dc06081544e944efafcd5caf
kitamura04111 <r.kitamura@kmonos-design.com>
2024-07-10 16:53:04 +0900

    Update footer.php

--- wp/wp-content/themes/my_custom_theme/footer.php
+++ wp/wp-content/themes/my_custom_theme/footer.php
@@ -1,26 +1,15 @@
-  <!--footer st-->
   <footer>
     <h2 class="name-ja">社会保険労務士きたむら事務所</h2>
-    <p class="name-en">KITAMURA<br>
-      Labor and Social<br>
-      Security Attorney</p>
-  
-    <p>社会保険労務士　北村広美</p>  
-
-    
+    <p class="name-en">KITAMURA<br>Labor and Social<br>Security Attorney</p>
+    <p>社会保険労務士　北村広美</p>
     <p class="footer-time">営業時間　平日9:00～18:00</p>
-    <!-- <p class="footer-address">住所　〒150-0031　東京都渋谷区桜丘町23番17号シティコート桜丘408</p> -->
     <p class="footer-tel">TELL 03-6822-8544</p>
-    <p class="copy"><small>&copy; 2024 社会保険労務士きたむら事務所</small></p>
-    <a class="scroll-top" href="#top"><span><img src="img/link-up.svg" alt="scrollbtn"></span></a>
+    <p class="copy"><small>&copy; <?php echo date('Y'); ?> 社会保険労務士きたむら事務所</small></p>
+    <a class="scroll-top" href="#top"><span><img src="<?php echo get_template_directory_uri(); ?>/img/link-up.svg" alt="scrollbtn"></span></a>
   </footer>
-  <!-- footer ed -->
-
-  <!-- jquery-CDN -->
-  <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
-    crossorigin="anonymous"></script>
-  <script src="js/index.js"></script>
   <?php wp_footer(); ?>
+  <!-- jquery-CDN -->
+  <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/index.js"></script>
 </body>
-
 </html>
